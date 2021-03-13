21f72d2ec5361f2ba380ee08a50040fadf204519
ayame0921 <hasegawaayame10@gmail.com>
2021-03-13 16:18:19 +0900

    ecsite

--- ecsite/app/views.py
+++ ecsite/app/views.py
@@ -19,8 +19,8 @@ def signup(request):
         if form.is_valid():
             new_user = form.save()
             input_email = form.cleaned_data['email']
-            input_password = form.cleaned_data['password']
-            new_user = authenticate(email=input_email,password=input_password)
+            input_password = form.cleaned_data['password1']
+            new_user = authenticate(email=input_email, password=input_password)
             if new_user is not None:
                 login(request, new_user)
                 return redirect('app:index')
